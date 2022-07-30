09bb785f97166d2d18fa74d841dd4d26140ac7de
Braiam Tejeda <braiam.tejeda@gmail.com>
2022-07-30 16:37:58 -0500

    Fin Seccion 9

---
+++ src/06-memos/CallbackHook.jsx
@@ -0,0 +1,26 @@
+import { useCallback, useState } from 'react';
+import { ShowIncrement } from './ShowIncrement';
+
+export const CallbackHook = () => {
+
+    const [counter, setCounter] = useState(10);
+    const incrementFather = useCallback(
+        (value) => {
+            setCounter((c) => c + value);
+        },
+        [],
+    );
+
+    // const increment = () => {
+    //     setCounter(counter + 1);
+    // }
+
+    return (
+        <>
+            <h1>useCallbackHook Hook: { counter }</h1>
+            <hr />
+
+            <ShowIncrement increment={ incrementFather } />
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,12 @@ import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 import { SimpleForm } from './02-useEffect/SimpleForm';
 import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
+import { FocusScreen } from './04-useRef/FocusScreen';
+import { Layout } from './05-useLayoutEffect/Layout';
+import { CallbackHook } from './06-memos/CallbackHook';
+import { MemoHook } from './06-memos/MemoHook';
+import { Memorize } from './06-memos/Memorize';
+import { Padre } from './07-tarea-memo/Padre';
 import { HooksApp } from './HooksApp';
 import './index.css';
 
@@ -15,6 +21,12 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     // {/* <CounterWithCustomHook /> */}
     // <SimpleForm />
     // <FormWithCustomHook />
-    <MultipleCustomHooks />
+    // <MultipleCustomHooks />
+    // <FocusScreen />
+    // <Layout />
+    // <Memorize />
+    // <MemoHook />
+    // <CallbackHook />
+    <Padre />
   // </React.StrictMode>
 )
